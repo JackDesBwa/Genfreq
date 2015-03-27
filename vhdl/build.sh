3af5e898862237a234ce8c7d9fa9ebfaec2a7b0e
Mathieu Gaborit <mat.gaborit@gmx.com>
2015-03-27 21:51:45 +0100

    vhdl: add elaboration of primary unit to build process

--- vhdl/build.sh
+++ vhdl/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+primary_unit='top_level'
 ieee_standard='synopsys'
+flags="--std=02 --ieee=$ieee_standard"
 
+echo "Analysis..."
 for i in *.vhd; do
 	echo "====> $i"
-	ghdl -a --std=02 --ieee=$ieee_standard $i
+	ghdl -a $flags $i
 done
+
+
+echo ""
+echo "Elaborating primary unit : $primary_unit..."
+ghdl -e $flags $primary_unit && echo "DONE"
